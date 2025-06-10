afc2ecd6035a6a8fb8926ad344ba9b529b86bf81
Nirjus <karmakarnirjus4839@gmail.com>
2025-06-10 10:48:20 +0530

    made change to backend absolute url to relative url

--- frontend/src/utils/userOperation.js
+++ frontend/src/utils/userOperation.js
@@ -1,4 +1,5 @@
-const backendUrl = "http://127.0.0.1:5000";
+const backendUrl =
+  import.meta.env.MODE == "development" ? "http://127.0.0.1:5000" : "";
 
 export const getUsers = async () => {
   let result = { success: false, data: null, error: null, loading: true };
